aef8fbdd7a4a4e8dde24474b5f95ac72e605d03f
Coral Herrnandez <45517870+Esmeralda1997@users.noreply.github.com>
2021-06-04 15:45:03 -0500

    Confetti

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ myFunction = () => {
     
         const date = new Date
     
-        let bday = false
+        let b_day = false
     
         if(parseInt(date.getMonth())+1 == birthMonth && parseInt(date.getDate()) == birthDay){
-            bday = true
+            b_day = true
         }
     
         let age = parseInt(date.getFullYear()) - birthYear;
@@ -22,21 +22,49 @@ myFunction = () => {
             age--;
         }
     
-        let Bday = bday ? "Happy Birthday!!! " : ""
+        let Bday = b_day ? "Happy Birthday!!! " : ""
     
-        document.getElementById('output').innerHTML = Bday + "You are " + age + " years old."
+        document.getElementById('output').innerHTML = Bday + "You are " + age + " years old!"
     
-        if(bday)  {  
-            document.getElementById("cake-svg").style.visibility="visible";
+        if(b_day)  {  
+            document.getElementById("cake-svg").style.visibility="visible"
+            var duration = 15 * 1000;
+            var animationEnd = Date.now() + duration;
+            var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+            function randomInRange(min, max) {
+              return Math.random() * (max - min) + min;
+            }
+
+            var interval = setInterval(function() {
+              var timeLeft = animationEnd - Date.now();
+
+              if (timeLeft <= 0) {
+                return clearInterval(interval);
+              }
+
+              var particleCount = 50 * (timeLeft / duration);
+              // since particles fall down, start a bit higher than random
+              confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+              confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+            }, 250);
         }
+
+        document.getElementById("prompt").style.visibility = "hidden"
+        document.getElementById("bday").style.visibility = "hidden"
+        document.getElementById("myBtn").style.visibility="hidden"
+        document.getElementById("reset").style.visibility="visible"
+
     }
 
 }
 
-$("#bday").keypress(function(event) {
-    console.log( event );
-	if (event.keyCode === 13) {
-   		event.preventDefault();
-   		alert();
-    } 
-});
+myResetFunction = () => {
+    document.getElementById("prompt").style.visibility = "visible"
+    document.getElementById("bday").style.visibility = "visible"
+    document.getElementById("myBtn").style.visibility="visible"
+    document.getElementById("cake-svg").style.visibility = "hidden"
+    document.getElementById("output").innerHTML = ""
+    document.getElementById("reset").style.visibility="hidden"
+}
+
