dd233e699cd69cc1cb13108ca27cbd032f1f6aed
Coral Herrnandez <45517870+Esmeralda1997@users.noreply.github.com>
2021-06-04 10:45:44 -0500

    Required

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-
 myFunction = () => {
-    const birthDate = document.getElementById("b-day").value
+    const birthDate = document.getElementById("bday").value
 
-    const birthDay = parseInt(birthDate.substring(8, 10))
-    const birthYear = parseInt(birthDate.substring(0, 4))
-    const birthMonth = parseInt(birthDate.substring(5, 7))
-
-    const date = new Date
-
-    let bday = false
-
-    if(parseInt(date.getMonth())+1 == birthMonth && parseInt(date.getDate()) == birthDay){
-        bday = true
-    }
-
-    let age = parseInt(date.getFullYear()) - birthYear;
-
-    if(parseInt(date.getMonth())+1 <= birthMonth && parseInt(date.getDate()) < birthDay){
-        age--;
-    }
-
-    let Bday = bday ? "Happy Birthday!!! " : ""
-
-    document.getElementById('output').innerHTML = Bday + "You are " + age + " years old."
-
-    if(bday)  {  
-        document.getElementById("cake-svg").style.visibility="visible";
+    if(birthDate == NaN){
+        return;
+    } else {
+        const birthDay = parseInt(birthDate.substring(8, 10))
+        const birthYear = parseInt(birthDate.substring(0, 4))
+        const birthMonth = parseInt(birthDate.substring(5, 7))
+    
+        const date = new Date
+    
+        let bday = false
+    
+        if(parseInt(date.getMonth())+1 == birthMonth && parseInt(date.getDate()) == birthDay){
+            bday = true
+        }
+    
+        let age = parseInt(date.getFullYear()) - birthYear;
+    
+        if(parseInt(date.getMonth())+1 <= birthMonth && parseInt(date.getDate()) < birthDay){
+            age--;
+        }
+    
+        let Bday = bday ? "Happy Birthday!!! " : ""
+    
+        document.getElementById('output').innerHTML = Bday + "You are " + age + " years old."
+    
+        if(bday)  {  
+            document.getElementById("cake-svg").style.visibility="visible";
+        }
     }
 
 }
+
+$("#bday").keypress(function(event) {
+    console.log( event );
+	if (event.keyCode === 13) {
+   		event.preventDefault();
+   		alert();
+    } 
+});
